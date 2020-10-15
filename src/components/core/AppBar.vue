b0ef1f3167b4f5511831d2ef073352f940e5ecb3
limtaegeun <imori333@gmail.com>
2020-10-15 17:05:15 +0900

    init

---
+++ src/components/core/AppBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar color="white">
+    <v-icon large color="red">mdi-pinterest</v-icon>
+    <router-link to="/"><v-btn rounded dark>홈</v-btn></router-link>
+    <router-link to="/following"><v-btn rounded>팔로잉</v-btn></router-link>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="home-content">
+      <core-app-bar></core-app-bar>
+      <core-view></core-view>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+  // @ is an alias to /src
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+  export default {
+    name: 'Home',
+    components: {
+      CoreAppBar: () => import('@/components/core/AppBar.vue'),
+      CoreView: () => import('@/components/core/CoreView.vue'),
+    },
+  };
 </script>
+<style lang="scss" scoped>
+  .home {
+    background-color: #e7e7e7;
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+  }
+  .home-content {
+    background-color: white;
+    min-width: 560px;
+    min-height: 100vh;
+  }
+</style>
