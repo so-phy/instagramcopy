1ed783418e8b400dd9a89a6dd5923531b4bda1f6
limtaegeun <imori333@gmail.com>
2020-10-20 18:15:41 +0900

    add Post comp

---
+++ src/components/main/Post.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="post-component">
+    <v-row class="post-head" align="center" no-gutters>
+      <v-avatar size="40" class="head-avatar"
+        ><v-img :src="post.userProfile.img"></v-img
+      ></v-avatar>
+      <h3>{{ post.userProfile.name }}</h3>
+      <v-spacer></v-spacer>
+      <v-btn x-large icon><v-icon>mdi-dots-horizontal</v-icon></v-btn>
+    </v-row>
+    <div class="img-container">
+      <v-img
+        :src="post.content.img"
+        max-height="597"
+        min-height="479"
+        contain
+      ></v-img>
+    </div>
+    <post-icon></post-icon>
+    <like :like="post.like"></like>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Post',
+    props: ['post'],
+    components: {
+      PostIcon: () => import('@/components/main/PostIcon.vue'),
+      Like: () => import('@/components/main/Like.vue'),
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  .post-head {
+    height: 60px;
+    width: 100%;
+    border-bottom: 1px solid #80808054;
+    .head-avatar {
+      margin-left: 20px;
+    }
+    h3 {
+      font-size: 1.7rem;
+      font-weight: bold;
+      display: inline-block;
+      margin: 0 20px;
+    }
+  }
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <story-slider></story-slider>
+    <post v-for="(post, i) in posts" :key="i" :post="post"></post>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex';
   export default {
     name: 'Main',
     components: {
       StorySlider: () => import('@/components/main/StorySlider.vue'),
+      Post: () => import('@/components/main/Post.vue'),
+    },
+    computed: {
+      ...mapState(['posts']),
     },
   };
 </script>
